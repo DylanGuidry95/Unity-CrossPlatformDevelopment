7976581ba9949d2cf8d5f53168f06d9728a100ff
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-05-18 13:27:52 -0500

    Loot table practive

---
+++ Assets/Scripts/BetterLootTable.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[CreateAssetMenu(fileName = "BetterLootTable", menuName = "LootTable/Better")]
+public class BetterLootTable : ScriptableObject
+{
+    [System.Serializable]
+    public class LootDropEvent : UnityEvent<List<Item>> { }
+
+    [System.Serializable]
+    public class DroppableItem
+    {
+        public Item Drop;
+        [Range(0f,1f)]
+        public float DropChance;
+    }
+
+    public List<DroppableItem> Drops = new List<DroppableItem>();
+    public LootDropEvent lootDropEvent = new LootDropEvent();
+
+
+    public void DropItems()
+    {
+        List<Item> DroppedItems= new List<Item>();
+        float roll = Random.Range(0f, 1f);
+        Debug.Log(roll);
+        foreach (var item in Drops)
+        {
+            if(item.DropChance > roll)
+                DroppedItems.Add(item.Drop);
+        }
+
+        //Testing
+        foreach (var item in DroppedItems)
+        {
+            Debug.Log(item.DisplayName);
+        }
+        //End
+
+        lootDropEvent.Invoke(DroppedItems);
+    }    
+}
+
+#if UNITY_EDITOR
+[CustomEditor((typeof(BetterLootTable)))]
+public class InspectorBetterLootTable : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Drop Items"))
+        {
+            var refrence = target as BetterLootTable;
+            refrence.DropItems();
+        }
+    }
+}
+#endif
